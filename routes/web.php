f6ce92f3f85116b771965c8e4272089f8c20e976
Santhosh Kumar Rajagiri <seo.santhosh@gmail.com>
2023-08-01 16:37:16 +0530

    router where update

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,23 @@ use Illuminate\Support\Facades\Route;
 */
 // GET
 //Route::get('/blog', [postController::class, 'index']);
-Route::get('/blog/{$id}', [postController::class, 'show']);
+Route::get('/blog/{id}', [postController::class, 'show']);
+
+
+Route::get('/blog/{id}/{name}', [postController::class, 'show'])
+	->where([
+		'id'=>'[0-9]+',
+		'name'=>'[A-Za-z]+'
+		]);
 
 
 Route::get('/blog/create', [postController::class, 'create']);
 Route::post('/blog/', [postController::class, 'store']);
 
-Route::get('/blog/edit/{$id}', [postController::class, 'edit']);
-Route::patch('/blog/{$id}', [postController::class, 'update']);
+Route::get('/blog/edit/{id}', [postController::class, 'edit']);
+Route::patch('/blog/{id}', [postController::class, 'update']);
 
-Route::delete('/blog/{$id}', [postController::class, 'destroy']);
+Route::delete('/blog/{id}', [postController::class, 'destroy']);
 
 Route::match(['get','post'], '/blog', [postController::class, 'index']);
 
