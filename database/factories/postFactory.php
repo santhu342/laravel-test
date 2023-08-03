112a156b08abef9c3cecf8fe6134cd6711f089e4
Santhosh Kumar Rajagiri <seo.santhosh@gmail.com>
2023-08-03 13:18:09 +0530

    Update postFactory.php

--- database/factories/postFactory.php
+++ database/factories/postFactory.php
@@ -15,15 +15,13 @@ class postFactory extends Factory
     {
         return [
             //
-            [
                 'title' => $this->faker->unique()->sentence(),
-                'excerpt' => $this->faker->realText(5),
+                'excerpt' => $this->faker->realText(10),
                 'body' => $this->faker->text(),
                 'img_path' => $this->faker->imageUrl(640,480),
                 'is_published' => 1,
                 'min_to_read' => $this->faker->numberBetween(1,10),
                 'ip_address' => $this->faker->realText(15)
-            ],
         ];
     }
 }
