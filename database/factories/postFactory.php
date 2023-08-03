4df13867619b1edcf8c08f2b36b7a8cbff3616c0
Santhosh Kumar Rajagiri <seo.santhosh@gmail.com>
2023-08-03 13:06:04 +0530

    postFactory update

---
+++ database/factories/postFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class postFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            [
+                'title' => $this->faker->unique()->sentence(),
+                'excerpt' => $this->faker->realText(5),
+                'body' => $this->faker->text(),
+                'img_path' => $this->faker->imageUrl(640,480),
+                'is_published' => 1,
+                'min_to_read' => $this->faker->numberBetween(1,10),
+                'ip_address' => $this->faker->realText(15)
+            ],
+        ];
+    }
+}
