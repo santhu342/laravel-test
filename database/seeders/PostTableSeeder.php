3f45355579346bfd2635994a0648c629824dbd24
Santhosh Kumar Rajagiri <seo.santhosh@gmail.com>
2023-08-03 12:02:39 +0530

    post dummy data update

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -13,6 +13,23 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $posts = [
+            [
+                'title' => 'Post One',
+                'excerpt' => 'Summary of post one',
+                'body' => 'body one',
+                'img_path' => 'empty',
+                'is_published' => false,
+                'min_to_read' => 5,
+            ],
+            [
+                'title' => 'Post Two',
+                'excerpt' => 'Summary of post Two',
+                'body' => 'body Two',
+                'img_path' => 'empty',
+                'is_published' => false,
+                'min_to_read' => 5,
+            ],
+        ];
     }
 }
